2cf072f34aee4f2c6144adf3d97419e813d003a5
thodoxuan99 <tho.dok2017@hcmut.edu.vn>
2021-05-09 13:54:59 +0700

    add REQUIRES ds1307 to sun_calculator component

--- components/sun_calculator/sun_calculator.c
+++ components/sun_calculator/sun_calculator.c
@@ -1,5 +1,35 @@
 #include "sun_calculator.h"
 
+
+
+i2c_dev_t init_ds1307(){
+    i2c_dev_t dev;
+    while (ds1307_init_desc(&dev, I2C_NUM_0, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO) != ESP_OK) {
+        ESP_LOGE(pcTaskGetTaskName(0), "Could not init device descriptor.");
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+    return dev;
+}
+time_t get_time_ds1307( i2c_dev_t *dev){
+    
+    struct tm time;
+
+    while (ds1307_get_time(dev, &time) != ESP_OK) {
+        ESP_LOGE(pcTaskGetTaskName(0), "Could not get time.");
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+    time.tm_year = time.tm_year-1900;
+    time.tm_hour = time.tm_hour - TIME_ZONE_DS1307;
+    // printf( "time.tm_sec=%d",time.tm_sec);
+    // printf( "time.tm_min=%d",time.tm_min);
+    // printf( "time.tm_hour=%d",time.tm_hour);
+    // printf( "time.tm_wday=%d",time.tm_wday);
+    // printf( "time.tm_mday=%d",time.tm_mday);
+    // printf( "time.tm_mon=%d",time.tm_mon);
+    // printf( "time.tm_year=%d",time.tm_year);
+    return mktime(&time);
+}
+
 float toDays(time_t date){
     return toJulian(date) -J2000;
 }
