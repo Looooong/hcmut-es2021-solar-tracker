5b3a3e7c114f8202ac23b42f64dad1b1edf1ff60
thodoxuan99 <tho.dok2017@hcmut.edu.vn>
2021-06-01 11:21:10 +0700

    get back main.c

--- main/main.c
+++ main/main.c
@@ -1,15 +1,17 @@
-#include <cJSON.h>
-#include <ds1307.h>
+// #include <cJSON.h>
 #include <esp_log.h>
 #include <esp_system.h>
 #include <esp_websocket_client.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <stdio.h>
+#include "driver/adc.h"
+
 #include "cloud_client.h"
 #include "sun_calculator.h"
 #include "wifi_connector.h"
-#include "driver/adc.h"
+#include "rtc.h"
+
 
 typedef enum control_mode_t
 {
@@ -26,20 +28,20 @@ config_t;
 
 config_t config;
 
-i2c_dev_t init_ds1307();
-time_t get_time_ds1307(i2c_dev_t *dev);
-static void cloud_client_data_handler(const char *data, int length);
-
+// static void cloud_client_data_handler(const char *data, int length);
 void app_main(void)
 {
     ESP_ERROR_CHECK( nvs_flash_init() );
-    initialise_wifi();
-    i2c_dev_t dev = init_ds1307();
-    cloud_client_init(cloud_client_data_handler);
+    // initialise_wifi(&is_connected);
+    init_rtc();
+    // cloud_client_init(cloud_client_data_handler);
 
-    while (1)
+    while(true)
     {
-        time_t time = get_time_ds1307(&dev);
+        struct tm time_tm;
+        time_t time ;
+        get_time_rtc(&time_tm);
+        time = mktime(&time_tm);
         orientation_t orient = get_sun_orientation(time, 10.816572, 106.674488);
         printf("Time by Seconds : %ld\n", time);
         printf("azimuth = %f \ninclination = %f\n", orient.azimuth, orient.inclination);
@@ -77,30 +79,6 @@ static void cloud_client_data_handler(const char *data, int length)
     free(json_data);
 }
 
-i2c_dev_t init_ds1307()
-{
-    i2c_dev_t dev;
-    while (ds1307_init_desc(&dev, I2C_NUM_0, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO) != ESP_OK)
-    {
-        ESP_LOGE(pcTaskGetTaskName(0), "Could not init device descriptor.");
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
-    return dev;
-}
-
-time_t get_time_ds1307(i2c_dev_t *dev)
-{
-    struct tm time;
-
-    while (ds1307_get_time(dev, &time) != ESP_OK)
-    {
-        ESP_LOGE(pcTaskGetTaskName(0), "Could not get time.");
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
-    time.tm_year = time.tm_year - 1900;
-    time.tm_hour = time.tm_hour - TIME_ZONE_DS1307;
-    return mktime(&time);
-}
 void get_adc_value( ){
     adc1_config_width(ADC_WIDTH_BIT_12);
     adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_DB_0);
